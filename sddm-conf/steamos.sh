78977c58ae1924b3edcd4669421bb4039426c7db
TaYaKi71751 <shantian.tw@gmail.com>
2024-03-18 16:42:59 +0900

    Add mkdirp

--- sddm-conf/steamos.sh
+++ sddm-conf/steamos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo bash << EOF
+mkdir -p /etc/sddm.conf.d/
 echo "[Autologin]" > /etc/sddm.conf.d/steamos.conf
 echo "Relogin=true" >> /etc/sddm.conf.d/steamos.conf
 echo "User=${USER}" >> /etc/sddm.conf.d/steamos.conf
