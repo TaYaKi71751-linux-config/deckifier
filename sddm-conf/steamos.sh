ed8858fa4e3f2ee9f56c1dfab437cb99f3627c4f
TaYaKi71751 <shantian.tw@gmail.com>
2024-03-26 11:26:07 +0900

    Use gnome-xorg

--- sddm-conf/steamos.sh
+++ sddm-conf/steamos.sh
@@ -5,7 +5,7 @@ mkdir -p /etc/sddm.conf.d/
 echo "[Autologin]" > /etc/sddm.conf.d/steamos.conf
 echo "Relogin=true" >> /etc/sddm.conf.d/steamos.conf
 echo "User=${USER}" >> /etc/sddm.conf.d/steamos.conf
-echo "Session=plasmawayland" >> /etc/sddm.conf.d/steamos.conf
+echo "Session=gnome-xorg" >> /etc/sddm.conf.d/steamos.conf
 systemctl stop sddm
 pkill -9 wayland
 pkill -9 gamescope
