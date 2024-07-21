c700743842c89c79f4569af66704b2160ace02fe
TaYaKi71751 <shantian.tw@gmail.com>
2024-07-22 06:04:58 +0900

    Remove bin, profile.d

--- install.sh
+++ install.sh
@@ -3,7 +3,5 @@
 sudo pacman -Sy gamescope --noconfirm --overwrite '*'
 yay -Sy gamescope-session-git --noconfirm --overwrite '*'
 source ./autologin/autologin.sh
-source ./profile.d/gamescope.sh
 sudo cp wayland-sessions/* /usr/share/wayland-sessions/
-#sudo cp bin/* /usr/bin/
 source ./sddm-conf/steamos.sh
--- profile.d/deck-touchscreen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo bash << EOF
-echo "#!/bin/bash" > /etc/profile.d/deck-touchscreen.sh
-echo "xinput --map-to-output 'pointer:FTS3528:00 2808:1015' eDP" >> /etc/profile.d/deck-touchscreen.sh
-EOF
-
